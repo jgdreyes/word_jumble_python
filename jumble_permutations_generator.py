d47c6f81e50edbc2440a50d93846d8d3f28b8c31
jgdreyes <jgdreyes@gmail.com>
2014-06-22 18:26:42 -0700

    Longer word and ability to get all jumbles

--- jumble_permutations_generator.py
+++ jumble_permutations_generator.py
@@ -1,16 +1,63 @@
 class JumblePermutationsGenerator:
+    @staticmethod
+    def jumble(jumble_str):
+        generator = JumblePermutationsGenerator(jumble_str)
+        return generator.get_words()
+
+    @staticmethod
+    def accepted_letters(word):
+        letters = {
+            "a": True,
+            "b": True,
+            "c": True,
+            "d": True,
+            "e": True,
+            "f": True,
+            "g": True,
+            "h": True,
+            "i": True,
+            "j": True,
+            "k": True,
+            "l": True,
+            "m": True,
+            "n": True,
+            "o": True,
+            "p": True,
+            "q": True,
+            "r": True,
+            "s": True,
+            "t": True,
+            "u": True,
+            "v": True,
+            "w": True,
+            "x": True,
+            "y": True,
+            "z": True
+        }
+        accepted_letters = []
+        for letter in word.lower():
+            if letter in letters:
+                accepted_letters.append(letter)
+        return accepted_letters
+
     def __init__(self, jumble_str):
         self.jumble_str = jumble_str
-        self.letters = tuple(self.jumble_str)
+        self.letters = JumblePermutationsGenerator.accepted_letters(jumble_str)
         self.string_length = len(self.letters)
+        self.word_list = {}
         self._setup_dictionary()
 
-    def jumbles_in_length_of(self, pick):
+    def get_words(self):
+        self._append_to_word_list(''.join(self.letters))
+        for jumble_length in range(2, self.string_length):
+            self.append_jumbles_in_length_of(jumble_length)
+        return self.word_list.keys()
+
+    def append_jumbles_in_length_of(self, pick):
         n = self.string_length
         indices = range(n)
         cycles = range(n, n - pick, -1)
-        word_list = []
-        self._append_to_word_list(word_list, self.letters_to_word(indices[:pick]))
+        self._append_to_word_list(self.letters_to_word(indices[:pick]))
         while n:
             for i in reversed(range(pick)):
                 cycles[i] -= 1
@@ -20,10 +67,10 @@ class JumblePermutationsGenerator:
                 else:
                     j = cycles[i]
                     indices[i], indices[-j] = indices[-j], indices[i]
-                    self._append_to_word_list(word_list, self.letters_to_word(indices[:pick]))
+                    self._append_to_word_list(self.letters_to_word(indices[:pick]))
                     break
             else:
-                return word_list
+                return
 
     def letters_to_word(self, indices):
         letters = tuple(self.letters[i] for i in indices)
@@ -31,13 +78,14 @@ class JumblePermutationsGenerator:
 
     def _setup_dictionary(self):
         self.dictionary = {}
-        american_words = open('english-words.10', 'r')
+        american_words = open('all_english_words.txt', 'r')
         for word in american_words:
-            self.dictionary[word.strip()] = True
+            formatted_word = JumblePermutationsGenerator.accepted_letters(word.strip())
+            self.dictionary[''.join(formatted_word)] = True
 
-    def _append_to_word_list(self, word_list, word):
+    def _append_to_word_list(self, word):
         if self._is_word(word):
-            word_list.append(word)
+            self.word_list[word] = True
 
     def _is_word(self, word):
         return (word in self.dictionary)
